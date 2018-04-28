182ec1ab7a53656e4884492ed6d22c5b050843c7
adan_eslavo <adan_eslavo@o2.pl>
2018-04-28 19:11:37 +0200

    v0.1.1
    First plan put into sql file.

--- CompatibilityChanges.sql
+++ CompatibilityChanges.sql
@@ -1,1 +1,164 @@
-???
+Units:
+China (Xiafan Guanjun, (Frigate))
+Spoiler :
+Unlock at Navigation
+
+Morocco (Corsair -> Ship of the Line)
+Spoiler :
+set unitcombat to 35 (same as base SotL)
+Unlocks at Navigation
+400 production
+flagship promotion
+
+Spain (Armada (Corvette))
+Spoiler :
+Unlocks at Exploration
+350 production
+
+Greek (Klepht -> light infantry) --currently gatling gun
+Spoiler :
+Replace light infantry
+Unlocks at Fortification
+35RCS
+19 CS
+Light Skirmish promotion
+325 production
+
+Portugal (Cacadores -> light infantry) --currently gatling gun
+Spoiler :
+replaces light infantry
+unlocks at fortification
+Light Skirmish promotion
+375 production
+35 RCS
+21 CS
+
+Korea (Kobukseon >Carrack) -- currently caravel
+Spoiler :
+Replaces Carrack
+Unlocked at exploration
+3 movement (still has double move in coast)
+250 production
+38 CS
+
+Russia (Licorne > Field Gun)
+Spoiler :
+Field Gun replacement
+unlocked at Flintlock
+375 production
+35RCS
+18CS
+
+Siam (Seir Morb > Field Gun)
+Spoiler :
+Stays as VP field gun replacement (renamed Howitzer)
+3 Range
+
+Unit Tech Changes:
+Spoiler :
+Upgrades to changes:
+Qizilbash: Uhlan
+Hakkapeliitta: Uhlan
+Great Bombard: Field Gun
+Prowler: Light Infantry
+Langskib: Carrack
+Shotelai: Two-Handed Swordsman
+Licorne: Howitzer
+Cacadores: Gatling Gun
+Klepht: Gatling Gun
+Armada: Ship of the Line
+Fusta: Galleon
+Sofa: Light Infantry
+
+Obsolete Tech changes:
+Black Tug Warrior: Rifling
+Mandekalu Cavalry: Rifling
+Dromon: Warships
+Prau: Exploration
+Langskib: Navigation
+Shotelai: Flintlock
+Licorne: Ballistics
+Kobukseon: Warships
+Cacadores: Ballistics
+Klepht: Ballistics
+Prowler: Dynamite
+Armada: Industrialization
+Great Bombard: Rifling
+Qizilbash: Combustion
+Hakkapeliitta: Combustion
+Fusta: Warships
+Sofa: Dynamite
+
+
+Buildings:
+England (White Tower -> Tower of Buddhist Incense) -- currently hermitage
+Spoiler :
+
+UW: White Tower - Tower of Buddhist Incense Replacement
+Available at Sovereignty
+Requires Castle in City (instead of Manor and Garden)
++5:c5strength: City Defense
++3:c5culture: culture (up from +2)
++2:c5culture: culture to Castles on Empire
++3:c5gold: Gold to Constabulary in City
++3:c5food: Food to Arsenal in City
+1 Free Great Person of your choice
+-10% :c5unhappy:Religious needs modifier
+-25% spy stealing rate on empire
++1 :c5happy:Happiness for every enemy spy killed (permanent)
+2 :greatwork:Great works of Art slot, +3 :c5gold:Gold and +3 :c5culture:Culture if themed
+Comes with 1 free art: The Crown Jewels
+
+Dutch (Waag -> weigh house) -- currently bank
+Spoiler :
+
+UB: Waag - Weigh House
+Unlocked at Economics (1 tech earlier)
+Requires Market (instead of Bank)
+750:c5production: (down from 900)
+5 :c5gold:Gold (up from 3:c5gold:)
++3:c5gold: to Market in city
+1:c5citizen: Merchant Specialists Slot
++2% :c5greatperson: GPP and 2 :c5strength: CP in the city for every imported/exported luxury resource from other empires
+1:c5gold: Gold for every 2:c5citizen:pop (up from 1 for 4 pop)
+-10%:c5unhappy: poverty in city
+
+Austrian (Schutzenstand -> gunsmith) -- currently arsenal
+Spoiler :
+UB - Schutzenstand - Gunsmith
+Unlocks at Flintlock
+750:c5production:
+3:c5gold: maintenance
++3 :c5culture: Culture, +3 :c5production: Production
++30% :c5production: production to Gunpowder units and an additional 25XP for Gunpowder units
++15% :c5production:production to land military units
++2 :c5war:Supply cap
++1:c5production: Production and +1:c5culture: Culture for each allied CS
+
+French (Grande ecole -> academy) -- currently public school
+Spoiler :
+UB - Grande École (Public School) Available at Humanism
+Can be built in city with a salon (not mutually exclusive building)
+750:c5production: Prod
+5:c5gold: Maintenance (up from 3)
++3 :c5science: Science, +1 :c5culture: Culture, +2 :c5production: Production (up from 2 science)
+1 Scientist Specialist
++2 :c5science: Science & +1 :c5production: Production for every 5 :c5citizen: Citizens in the City (up from 1 science for every 5 citizens)
++2:c5production: Production for Scientist Specialist & +2 :c5science:Science for Engineers in city
+-20% :c5unhappy: Illiteracy in city
++2 :c5production:Production and +2 :c5science:Science at Computers.
+
+Brazil (Sambadrome -> Salon) --currently opera house
+Spoiler :
+
+UB: Samba School (Opera House)
+Available at Humanism
+Can be built in city with an academy (not mutually exclusive building)
+750:c5production: Prod
+3:c5gold: maintenance
++4:c5culture: Culture, +4:c5goldenage:GAP (up from +2:c5culture: only)
+1:c5culture: for every 6:c5citizen: population in city
+1 Musician specialist slot
+Musicians in city produce +1 :c5gold:gold and +2 :c5goldenage:GAP
++25% :c5goldenage:GAP during golden age
++2:tourism: Tourism at Radio
